53f9fdad9b232fc2e1bc14734b442d91972b1031
Felipe Trindade <felipe_biel3@hotmail.com>
2025-12-26 13:25:57 -0300

    Update db.js
    Atualização para deploy

--- backend_fornecedores/src/config/db.js
+++ backend_fornecedores/src/config/db.js
@@ -12,14 +12,4 @@ const db = mysql.createPool({
     }
 });
 
-// Mantivemos o teste de conexão simples para não quebrar o build
-db.getConnection((err, connection) => {
-    if (err) {
-        console.log('Aviso: Erro ao tentar conectar (pode ser normal durante o build):', err.message);
-    } else {
-        console.log('Conexão com o banco realizada com sucesso!');
-        connection.release();
-    }
-});
-
 export default db.promise();
